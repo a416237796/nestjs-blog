a10362aab41318cc767d62b9a7cf0f154bb3d502
wangsheng <a416237796@hotmail.com>
2019-08-28 16:11:34 +0800

    封装请求成功响应

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClassifyModule } from './classify/classify.module';
-import { ArticleModule } from './article/article.module';
+import { ClassifyModule } from './module/classify/classify.module';
+import { ArticleModule } from './module/article/article.module';
 import { UserModule } from './module/user/user.module';
 import { FileModule } from './module/file/file.module';
 
--- src/common/interceptor/transform.interceptor.ts
+++ src/common/interceptor/transform.interceptor.ts
@@ -1,9 +1,18 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class TransformInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+    return next.handle().pipe(
+      map((data: any) => {
+        return {
+          code: 0,
+          message: '请求成功',
+          data,
+        };
+      })
+    );
   }
 }
