7af6f69f85e57c7f24d965dcd83638a53d64736f
wangsheng <a416237796@hotmail.com>
2019-08-28 16:18:13 +0800

    封装数据验证

--- src/common/pipe/validation.pipe.ts
+++ src/common/pipe/validation.pipe.ts
@@ -1,8 +1,42 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  Injectable,
+  PipeTransform,
+  HttpException,
+  HttpStatus
+} from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import * as _ from 'lodash';
 
 @Injectable()
-export class ValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
+export class ValidationPipe implements PipeTransform<any> {
+  async transform(value: any, metadata: ArgumentMetadata) {
+    const { metatype } = metadata;
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      const errorMessage = errors.map(item => {
+        return {
+          currentValue: item.value,
+          [item.property]: _.values(item.constraints)[0],
+        };
+      });
+
+      throw new HttpException(
+        { message: errorMessage },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
     return value;
+  }
+
+  private toValidate(metatype: any): boolean {
+    const types = [String, Boolean, Number, Array, Object];
+    // return !types.find(type => metatype === type);
+    return !types.includes(metatype);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from './common/pipe/validation.pipe';
 import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -5,6 +6,7 @@ import { TransformInterceptor } from './common/interceptor/transform.interceptor
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
