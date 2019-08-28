6ba33f00ae05f0142ef809eb6e8e5195c62c8a16
wangsheng <a416237796@hotmail.com>
2019-08-28 16:03:59 +0800

    添加分类模块，用户模块，文章模块，文件上传模块，请求响应类，数据验证类，日志记录类，错误响应类

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClassifyModule } from './classify/classify.module';
+import { ArticleModule } from './article/article.module';
+import { UserModule } from './module/user/user.module';
+import { FileModule } from './module/file/file.module';
 
 @Module({
-  imports: [],
+  imports: [ClassifyModule, ArticleModule, UserModule, FileModule],
   controllers: [AppController],
   providers: [AppService],
 })
