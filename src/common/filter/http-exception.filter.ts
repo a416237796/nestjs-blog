8e4540e8bd3af17e3127fca05d8ea998fa324a1d
wangsheng <a416237796@hotmail.com>
2019-08-28 16:16:29 +0800

    封装错误响应

--- src/common/filter/http-exception.filter.ts
+++ src/common/filter/http-exception.filter.ts
@@ -1,6 +1,38 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import * as dayjs from 'dayjs'
 @Catch()
 export class HttpExceptionFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {}
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+    const status = 
+      exception instanceof HttpException
+      ? exception.getStatus()
+      : HttpStatus.INTERNAL_SERVER_ERROR;
+    const message =
+      exception.message ||
+      exception.message.message ||
+      exception.message.error ||
+      null;
+    Logger.log(message, '错误提示');
+    const timestamp = dayjs().format("YYYY-MM-DD HH:mm:ss");
+    const errorResponse = {
+      status,
+      data: message,
+      message: '请求失败！',
+      code: 1,
+      path: request.url,
+      method: request.method,
+      timestamp,
+    };
+    Logger.error(
+      `${timestamp}: ${request.method} ${request.url}`,
+      JSON.stringify(errorResponse),
+      'HttpExceptionFilter',
+    );
+    response
+      .status(status)
+      .json(errorResponse);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
